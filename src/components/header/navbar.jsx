95d78f2017391c74bbb76a6a17e4b9d368b835e4
pyam-bee <pamshre55@gmail.com>
2025-11-19 19:15:05 +0545

    navbar -animations -banner

--- src/components/header/navbar.jsx
+++ src/components/header/navbar.jsx
@@ -4,13 +4,13 @@ const navbar = () => {
   return (
     <div className='flex w-full justify-between items-center px-5 py-3 border-b'>
       <div className='text-4xl font-family-chromeslab px-10'>
-        POSTURA
+        <NavLink to="/">POSTURA</NavLink>
       </div>
       <nav className="flex gap-15 px-20">
-        <NavLink to="/" className="text-sm font-helvetica-light hover-underline">Home</NavLink>
-        <NavLink to="/prints&posters" className="text-sm font-helvetica-light hover-underline">Prints & Posters</NavLink>
-        <NavLink to="/frame" className="text-sm font-helvetica-light hover-underline">Frame</NavLink>
-        <NavLink to="/about" className="text-sm font-helvetica-light hover-underline">About</NavLink>
+        <NavLink to="/" className="text-base font-helvetica-light underline-animation">Home</NavLink>
+        <NavLink to="/prints&posters" className="text-base font-helvetica-light underline-animation">Prints & Posters</NavLink>
+        <NavLink to="/frame" className="text-base font-helvetica-light underline-animation">Frame</NavLink>
+        <NavLink to="/about" className="text-base font-helvetica-light underline-animation">About</NavLink>
       </nav>
     </div>
   )
--- src/pages/home/page.jsx
+++ src/pages/home/page.jsx
@@ -1,8 +1,23 @@
 const Home = () => {
   return (
-      <div>
-        Home
-        <p>Products here</p>
+      <div className="relative w-full h-full">
+        <div className="border-b">
+          <img className="w-full" src="/src/assets/sale/banner.png" alt="banner" />
+        </div>
+
+        <div className="flex item-center justify-center pt-5">
+          <div className="pb-2">
+            <h2 className="text-3xl font-helvetica-medium uppercase
+              after:content-[''] 
+              after:absolute 
+              after:left-1/2 
+              after:bottom-0 
+              after:-translate-x-1/2 
+              after:h-[1px] 
+              after:w-[9.5%] 
+              after:bg-black">best seller</h2>
+          </div>
+        </div>
       </div>
   )
 }
